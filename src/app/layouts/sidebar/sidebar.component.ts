8e101ccd9307d4abd920e9b85b93600de4a5b857
Phạm Tiến Dũng <56197293+phamtiendungcw@users.noreply.github.com>
2023-02-17 23:41:01 +0700

    update link profile for sidebar

--- src/app/layouts/sidebar/sidebar.component.ts
+++ src/app/layouts/sidebar/sidebar.component.ts
@@ -2,15 +2,14 @@
  * Copyright (c) 2023. Phạm Tiến Dũng (DungCW)
  */
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AccountService } from '../../service/account.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class SidebarComponent {
+  constructor(public accountService: AccountService) {}
 }
