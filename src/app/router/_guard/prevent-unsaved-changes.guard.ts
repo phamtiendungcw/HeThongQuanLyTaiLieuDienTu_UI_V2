cb05c1c72d91ce3f028731622d32f0f6668f719d
Phạm Tiến Dũng <56197293+phamtiendungcw@users.noreply.github.com>
2023-02-27 13:58:26 +0700

    Fixed profile edit

--- src/app/router/_guard/prevent-unsaved-changes.guard.ts
+++ src/app/router/_guard/prevent-unsaved-changes.guard.ts
@@ -3,20 +3,16 @@
  */
 
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanDeactivate } from '@angular/router';
 import { PageProfileComponent } from '../../pages/page-profile/page-profile.component';
 
 @Injectable({
   providedIn: 'root',
 })
-export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
-  canDeactivate(
-    component: PageProfileComponent,
-    currentRoute: ActivatedRouteSnapshot,
-    currentState: RouterStateSnapshot,
-    nextState?: RouterStateSnapshot
-  ): Observable<boolean> | boolean {
+export class PreventUnsavedChangesGuard
+  implements CanDeactivate<PageProfileComponent>
+{
+  canDeactivate(component: PageProfileComponent): boolean {
     if (component.editForm?.dirty) {
       return confirm(
         'Bạn có chắc chắn muốn thoát khỏi trang này? Mọi thay đổi chưa được lưu sẽ bị mất'
